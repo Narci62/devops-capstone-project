92f5bddb67015806242df19d1a0fe5c2dffbe2bd
KODONOU Narcisse <narcissekodonou@gmail.com>
2025-04-21 05:57:35 -0400

    update linting with flake8

--- service/routes.py
+++ service/routes.py
@@ -61,7 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
-# ... place you code here to LIST accounts ...
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -90,7 +90,7 @@ def get_accounts(account_id):
     Reads an Account
     This endpoint will read an Account based the account_id that is requested
     """
-    
+
     app.logger.info("Request to read an account with id: %s", account_id)
 
     account = Account.find(account_id)
@@ -103,7 +103,7 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -116,7 +116,7 @@ def update_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
+
     account.deserialize(request.get_json())
     account.update()
 
@@ -125,6 +125,7 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
